5e058f787515b7534a45ce6801714b620523ab4a
Leonard.Fetzer <lefe00007@stud.uni-saarland.de>
2025-09-18 21:20:14 +0200

    CloudAct&moveCloudsLogicWithoutLogger

--- src/main/kotlin/de/unisaarland/cs/se/selab/clouds/CloudHandler.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/clouds/CloudHandler.kt
@@ -3,7 +3,6 @@ package de.unisaarland.cs.se.selab.clouds
 import de.unisaarland.cs.se.selab.board.BoardData
 import de.unisaarland.cs.se.selab.board.Tile
 
-
 class CloudHandler (val cloudData: CloudData, val board: BoardData) {
     //
     fun moveClouds() {
@@ -12,14 +11,23 @@ class CloudHandler (val cloudData: CloudData, val board: BoardData) {
     }
     //
     private fun cloudAct(cloud: Cloud) {
-        TODO()
+        while (cloud.stepsRemaining > 0) {
+            cloud.stepsRemaining -= 1
+            if (rainIfPossible(cloud) || moveOneIfPossible(cloud)) {break}
+        }
     }
     //
-    private fun rainIfPossible(cloud: Cloud) {
-        TODO()
+    private fun rainIfPossible(cloud: Cloud) : Boolean {
+        // Returns true if Dissipate
+        var amount = cloud.waterAmount
+        if (amount < 5000) {return false}
+        else {
+            TODO()
+        }
     }
     //
-    private fun moveOneIfPossible(cloud: Cloud) {
+    private fun moveOneIfPossible(cloud: Cloud) : Boolean {
+        // Returns true if hitsVillage or Merged or CantMove
         TODO()
     }
     //

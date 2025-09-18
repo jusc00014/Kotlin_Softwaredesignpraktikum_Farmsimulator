fae43bb13cca81e3836d25d6544d359cb1cad084
Leonard.Fetzer <lefe00007@stud.uni-saarland.de>
2025-09-18 16:23:10 +0200

    mutableIterate

--- src/main/kotlin/de/unisaarland/cs/se/selab/clouds/CloudHandler.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/clouds/CloudHandler.kt
@@ -6,7 +6,11 @@ import de.unisaarland.cs.se.selab.board.Tile
 
 class CloudHandler {
     //
-    fun moveClouds(clouds: CloudData, board: BoardData) {
+    fun moveClouds(cloudData: CloudData, board: BoardData) {
+        TODO()
+    }
+    //
+    private fun cloudAct() {
         TODO()
     }
     //
@@ -20,6 +24,16 @@ class CloudHandler {
     //
     private fun neighbor(tileId: Int, board: BoardData) : Tile? {
         TODO()
+    }
+    //
+    private fun <T: Any> mutableIterate(list: MutableList<T>, action: (T) -> Unit) {
+        // Necessary for List altering within their very own Iteration
+        val processed = mutableSetOf<T>()
+        while (true) {
+            val next = list.firstOrNull { it !in processed } ?: break
+            processed.add(next)
+            action(next)
+        }
     }
     //
 }
--- src/main/kotlin/de/unisaarland/cs/se/selab/incidents/CloudCreation.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/incidents/CloudCreation.kt
@@ -7,13 +7,13 @@ import de.unisaarland.cs.se.selab.clouds.CloudData
 const val STEP_DEFAULT = 10
 
 class CloudCreation(id: Int, tick: Int, private val duration: Int, private val amount: Int,
-                    private val tiles: Set<Tile>, private val clouds: CloudData) : Incident(id, tick) {
+                    private val tiles: Set<Tile>, private val cloudData: CloudData) : Incident(id, tick) {
     //
     override fun execute() {
         for(tile in tiles) {
             if (tile.type != TileType.VILLAGE) {
-                val newCloud = clouds.createCloud(duration, amount, tile.id, STEP_DEFAULT)
-                clouds.mergeIfNecessary(newCloud)
+                val newCloud = cloudData.createCloud(duration, amount, tile.id, STEP_DEFAULT)
+                cloudData.mergeIfNecessary(newCloud)
             }
         }
     }

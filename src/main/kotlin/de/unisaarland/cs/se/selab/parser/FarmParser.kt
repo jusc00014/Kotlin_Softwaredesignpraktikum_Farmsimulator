ef813e91a9bb5afe4285ec7c1f101561cfe6a5e8
cheb00001 <chrebel1612@gmail.com>
2025-09-18 09:25:16 +0200

    skeleton for plant and farmparser

---
+++ src/main/kotlin/de/unisaarland/cs/se/selab/parser/FarmParser.kt
@@ -0,0 +1,76 @@
+import org.json.JSONArray
+import org.json.JSONObject
+
+class FarmParser {
+    private val machine_names = mutableListOf<String>()
+    private val finished_machines = mutableMapOf<Int, String>()
+
+    fun parse(jsonFile: String, board: BoardData): (List<Farm>, Map<Int, Machine>) {
+
+    }
+
+
+    private fun vallidate_farm_id_and_name(id: Int, name: String): (Int, String) {
+
+    }
+
+    private fun validate_farmstead(id_list: MutableList<Int>, farm_id: Int): MutableList<Int> {
+
+    }
+
+    private fun validate_fields(id_list: MutableList<Int>, farm_id: Int): MutableList<Int> {
+
+    }
+
+    private fun validate_plantations(id_list: MutableList<Int>, farm_id: Int): MutableList<Int> {
+
+    }
+
+    private fun validate_at_least_one_field_or_plantation_tile(fields: MutableList<Int>, plantations: MutableList<Int>) {
+
+    }
+
+    private fun validate_sowing_plan_id_and_tick(id: Int, tick: Int): Pair<Int, Int> {
+
+    }
+
+    private fun validate_sowing_plan_plant_types(plant: String): PlantType {
+
+    }
+
+    private fun validate_sowing_plan_fields_by_radius(tile_id: Int, radius: Int): MutableList<Int> {
+
+    }
+
+    private fun validate_machines(farm_id: Int, machines: JSONArray, board: BoardData): List<Machine> {
+
+    }
+
+    private fun validate_machine_id_and_name(id: Int, name: String): Pair<Int, String> {
+
+    }
+
+    private fun validate_machine_actions(actions: Array<String>): Array<Action> {
+
+    }
+
+    private fun validate_machine_plants(plants: Array<String>): Array<PlantType> {
+
+    }
+
+    private fun validate_machine_duration(duration: Int): Int {
+
+    }
+
+    private fun validate_machine_tile(tile_id: Int): Int {
+
+    }
+
+    private fun add_machine_to_lists(current_farm_machines: MutableList<Machine>) {
+
+    }
+
+    private fun add_to_list(current_farm: Farm, farms: MutableList<Farm>) {
+
+    }
+}
---
+++ src/main/kotlin/de/unisaarland/cs/se/selab/plants/Plant.kt
@@ -0,0 +1,85 @@
+class Plant(type: PlantType, data: PlantData) {
+    private var pollinationEffect = 0.0
+    private var harvestEstimate = 0
+    private var sowTime = -1
+    private var harvestTime = -1
+    private var actionPerformed = null
+    private var stampedeCount = 0
+    private var mowedFor = 0
+
+    private fun harvestPenalty(): Double {
+
+    }
+
+    fun getHarvestEstimate(): Int {
+        return harvestEstimate
+    }
+
+    fun getMinMoisture(): Int {
+
+    }
+
+    private fun blooming(year_tick: Int): Boolean {
+
+    }
+
+    fun pollinateable(year_tick: Int): Boolean {
+
+    }
+
+    fun addPollination(power: Double) {
+        pollinationEffect = power
+    }
+
+    fun addStampede(): Boolean {
+
+    }
+
+    fun updateHarvestEstimate(year_tick: Int,
+                              drought: Boolean,
+                              sunlight: Int,
+                              moisture: Int,
+                              tileID: Int) {
+
+    }
+
+    fun isFallow(year_tick: Int): Boolean {
+
+    }
+
+    fun harvestable(year_tick: Int): Boolean {
+
+    }
+
+    fun cuttable(year_tick: Int): Boolean {
+
+    }
+
+    fun mowable(year_tick: Int): Boolean {
+
+    }
+
+    fun weedable(year_tick: Int): Boolean {
+
+    }
+
+    private fun harvest(year_tick: Int): Int {
+
+    }
+
+    fun performAction(action: Action): Int? {
+
+    }
+
+    fun isSown(): Boolean {
+        return (harvestEstimate > 0) && (type == PlantType.FIELD)
+    }
+
+    fun sow(plantType: PlantType, plantData: PlantData, year_tick: Int) {
+
+    }
+
+    private fun resetForNextTick() {
+
+    }
+}

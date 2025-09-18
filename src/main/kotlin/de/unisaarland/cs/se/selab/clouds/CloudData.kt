1493dd6ef416dbbf6f678943e36348a2d5ee3e07
Leonard.Fetzer <lefe00007@stud.uni-saarland.de>
2025-09-18 11:15:26 +0200

    CloudDataSkelli

--- src/main/kotlin/de/unisaarland/cs/se/selab/clouds/CloudData.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/clouds/CloudData.kt
@@ -1,4 +1,21 @@
 package de.unisaarland.cs.se.selab.clouds
 
-class CloudData {
+class CloudData(private var maxId: Int, public val idToCloud: MutableMap<Int, Cloud>) {
+    //
+    public fun mergeIfNecessary(cloudId: Int) {
+        TODO()
+    }
+    //
+    private fun checkOccupied(cloud: Cloud) : Cloud? {
+        TODO()
+    }
+    //
+    private fun createMergedCloud(cloud1: Cloud, cloud2: Cloud) : Cloud {
+        TODO()
+    }
+    //
+    public fun createCloud(duration: Int, amount: Int, location: Int) {
+        TODO()
+    }
+    //
 }

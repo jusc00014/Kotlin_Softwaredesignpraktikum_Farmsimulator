1136aef82fc5a550bb845ab54a282e76fabf12ad
Leonard.Fetzer <lefe00007@stud.uni-saarland.de>
2025-09-18 12:54:13 +0200

    CloudDataLogicWithoutLogger

--- src/main/kotlin/de/unisaarland/cs/se/selab/clouds/CloudData.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/clouds/CloudData.kt
@@ -1,6 +1,9 @@
 package de.unisaarland.cs.se.selab.clouds
 
-class CloudData(private var maxId: Int, public val idToCloud: MutableMap<Int, Cloud>) {
+import kotlin.math.max
+import kotlin.math.min
+
+class CloudData(private var maxId: Int, val clouds: MutableList<Cloud>) {
     //
     fun mergeIfNecessary(cloud: Cloud) : Boolean {
         val possibleDupe = checkOccupied(cloud)
@@ -8,24 +11,27 @@ class CloudData(private var maxId: Int, public val idToCloud: MutableMap<Int, Cl
             return false
         }
         else {
-            val newCloud = createMergedCloud(cloud, possibleDupe)
-            idToCloud[newCloud.id] = newCloud
-            idToCloud.remove(cloud.id)
-            idToCloud.remove(possibleDupe.id)
+            createMergedCloud(cloud, possibleDupe)
             return true
         }
     }
     //
     private fun checkOccupied(cloud: Cloud) : Cloud? {
-        return (idToCloud.values.firstOrNull { (it.id != cloud.id) && (it.location == cloud.location) })
+        return (clouds.firstOrNull { (it.id != cloud.id) && (it.location == cloud.location) })
     }
     //
-    private fun createMergedCloud(cloud1: Cloud, cloud2: Cloud) : Cloud {
-        TODO()
+    private fun createMergedCloud(cloud1: Cloud, cloud2: Cloud) {
+         createCloud(min(cloud1.duration, cloud2.duration), cloud1.waterAmount + cloud2.waterAmount, cloud1.location,
+            max(cloud1.stepsRemaining, cloud2.stepsRemaining)
+        )
+        clouds.remove(cloud1)
+        clouds.remove(cloud2)
     }
     //
-    fun createCloud(duration: Int, amount: Int, location: Int) {
-        TODO()
+    fun createCloud(duration: Int, amount: Int, location: Int, stepsRemaining: Int) {
+        val cloud = Cloud(maxId, duration, location, amount, stepsRemaining)
+        maxId += 1
+        clouds.add(cloud)
     }
     //
 }

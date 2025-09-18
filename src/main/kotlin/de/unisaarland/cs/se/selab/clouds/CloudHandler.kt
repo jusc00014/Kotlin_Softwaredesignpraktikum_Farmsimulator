ac661fce7b483b3e860036ea0e587ef124dc0b81
Leonard.Fetzer <lefe00007@stud.uni-saarland.de>
2025-09-18 11:20:59 +0200

    CloudHandlerSkelli

--- src/main/kotlin/de/unisaarland/cs/se/selab/clouds/CloudHandler.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/clouds/CloudHandler.kt
@@ -1,4 +1,24 @@
 package de.unisaarland.cs.se.selab.clouds
 
+import board.BoardData
+import board.Tile
+
 class CloudHandler {
+    //
+    public fun moveClouds(clouds: CloudData, board: BoardData) {
+        TODO()
+    }
+    //
+    private fun rain(cloud: Cloud, tile: Tile) {
+        TODO()
+    }
+    //
+    private fun moveOne() {
+        TODO()
+    }
+    //
+    private fun neighbor(tileId: Int, board: BoardData) : Tile? {
+        TODO()
+    }
+    //
 }

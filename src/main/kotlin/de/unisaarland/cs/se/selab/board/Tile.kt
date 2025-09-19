5bfb5013e975af57eef7aca17751c4098fe17747
Julian Gromov <jugr00003@stud.uni-saarland.de>
2025-09-19 10:32:52 +0200

    Changed Tile to handle rain
    - Tile: now has open fun rain
    - Fertile: increaseMoisture -> rain & irrigate

--- src/main/kotlin/de/unisaarland/cs/se/selab/board/Tile.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/board/Tile.kt
@@ -8,7 +8,18 @@ open class Tile(
     val farmID: Int? = null,
     val type: TileType
 ) {
+    /**
+     * Checks if the Tile is Fertile
+     */
     open fun isFertile(): Boolean {
         return false
+    }
+
+    /**
+     * Increases moisture until amount is used or capacity is reached
+     * @return the remaining amount
+     */
+    open fun rain(amount: Int): Int {
+        return 0
     }
 }

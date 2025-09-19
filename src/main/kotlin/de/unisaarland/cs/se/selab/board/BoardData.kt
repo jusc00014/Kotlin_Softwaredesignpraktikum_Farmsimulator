f547911c70fcd9a622212ab3ecca6b0870e3b2d7
Julian Gromov <jugr00003@stud.uni-saarland.de>
2025-09-19 12:06:50 +0200

    BoardData.kt
    - Reworked BoardData.neighbours and getFertiles

--- src/main/kotlin/de/unisaarland/cs/se/selab/board/BoardData.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/board/BoardData.kt
@@ -9,13 +9,37 @@ class BoardData(idToTile: Map<Int, Tile>) {
     private val idToTile: SortedMap<Int, Tile> = idToTile.toSortedMap()
     private val coordToId: Map<Coordinate, Int> = idToTile.values.associate { Pair(it.coord, it.id) }
 
-    private fun sameDirectNeighbours(coord: Coordinate): Set<Coordinate> {
-        return setOf(
-            Coordinate(coord.x + 1, coord.y + 1),
-            Coordinate(coord.x + 1, coord.y - 1),
-            Coordinate(coord.x - 1, coord.y + 1),
-            Coordinate(coord.x - 1, coord.y - 1),
+    private fun coordNeighbours(radius: Int, coordinate: Coordinate): Set<Coordinate> {
+        // Direction NW, NE, SE, SW
+        val coords = mutableSetOf<Coordinate>(
+            Coordinate(coordinate.x - 1, coordinate.y - 1),
+            Coordinate(coordinate.x + 1, coordinate.y - 1),
+            Coordinate(coordinate.x + 1, coordinate.y + 1),
+            Coordinate(coordinate.x - 1, coordinate.y + 1),
         )
+
+        // Direction N, E, S, W
+        if (coordinate.x % 2 == 0) {
+            coords.addAll(
+                setOf(
+                    Coordinate(coordinate.x, coordinate.y + 2),
+                    Coordinate(coordinate.x + 2, coordinate.y),
+                    Coordinate(coordinate.x, coordinate.y - 2),
+                    Coordinate(coordinate.x - 2, coordinate.y),
+                )
+            )
+        }
+
+        if (radius > 1) {
+            val coordsTemp = mutableSetOf<Coordinate>()
+            for (coord in coords) {
+                coordsTemp.addAll(coordNeighbours(radius - 1, coord))
+            }
+            coordsTemp.remove(coordinate)
+            coords.addAll(coordsTemp)
+        }
+
+        return coords
     }
 
     /**
@@ -26,19 +50,10 @@ class BoardData(idToTile: Map<Int, Tile>) {
             return emptyList()
         }
 
-        val evenCoordinates = mutableSetOf<Coordinate>()
-        val oddCoordinates = sameDirectNeighbours(tile.coord).toMutableSet()
-        val temp = mutableSetOf<Coordinate>()
-        repeat(radius) {
-            temp.clear()
-            evenCoordinates.forEach { temp.addAll(sameDirectNeighbours(it)) }
-            evenCoordinates.addAll(temp)
-            temp.clear()
-            oddCoordinates.forEach { temp.addAll(sameDirectNeighbours(it)) }
-            oddCoordinates.addAll(temp)
-        }
-
-        return (evenCoordinates + oddCoordinates).mapNotNull { getTileByCoord(it) }.toList()
+        return coordNeighbours(radius, tile.coord)
+            .filter { it != tile.coord }
+            .mapNotNull { getTileByCoord(it) }
+            .sortedBy { it.id }
     }
 
     /**
@@ -59,6 +74,6 @@ class BoardData(idToTile: Map<Int, Tile>) {
      * Returns all Fertile tiles mapped to their IDs
      */
     fun getFertiles(): Map<Int, Fertile> {
-        return idToTile.filterValues { it.isFertile() }.mapValues { it as Fertile }
+        return idToTile.values.mapNotNull { it.asFertile() }.associateBy { it.id }
     }
 }

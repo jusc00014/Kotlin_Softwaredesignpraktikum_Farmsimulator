9ad654d6c6c3d8004ff20858224f4cc53b7d35a2
Leonard.Fetzer <lefe00007@stud.uni-saarland.de>
2025-09-26 14:51:25 +0200

    illremain

---
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/cloudtests/CloudTestIllRemain.kt
@@ -0,0 +1,31 @@
+package de.unisaarland.cs.se.selab.systemtest.selab25.cloudtests
+
+import de.unisaarland.cs.se.selab.systemtest.selab25.utils.LogLevel
+import de.unisaarland.cs.se.selab.systemtest.selab25.utils.TestExtension
+
+/**
+ * Detekt
+ */
+class CloudTestIllRemain : TestExtension() {
+    override val name = "CloudTest2"
+    override val description = "Tests PostMovement CloudBehavior."
+
+    override val farms = "cloudTest/farms.json"
+    override val scenario = "cloudTest/scenario2.json"
+    override val map = "cloudTest/map.json"
+
+    override val logLevel = "DEBUG"
+    override val maxTicks = 1
+    override val startYearTick = 15
+
+    override suspend fun run() {
+        skipUntilLogType(LogLevel.INFO, "Cloud Movement")
+        assertCurrentLine("[INFO] Cloud Movement: Cloud 1 with 3000 L water moved from tile 2 to tile 5.")
+        assertNextLine("[INFO] Cloud Movement: Cloud 2 with 3000 L water moved from tile 3 to tile 5.")
+        assertNextLine(
+            "[IMPORTANT] Cloud Union: Clouds 1 and 2 united to cloud 3 with 6000 L water and duration 1 on tile 5."
+        )
+        assertNextLine("[IMPORTANT] Cloud Rain: Cloud 3 on tile 5 rained down 70 L water.")
+        assertNextLine("[INFO] Cloud Dissipation: Cloud 3 dissipates on tile 5.")
+    }
+}

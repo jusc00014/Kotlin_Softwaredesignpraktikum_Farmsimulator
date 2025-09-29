d7576f44a296da914e406f181144f9716b0449bb
Leonard.Fetzer <lefe00007@stud.uni-saarland.de>
2025-09-29 11:17:51 +0200

    DroughtRework

--- src/main/kotlin/de/unisaarland/cs/se/selab/incidents/Drought.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/incidents/Drought.kt
@@ -9,13 +9,10 @@ import de.unisaarland.cs.se.selab.logger.Logger
 class Drought(id: Int, tick: Int, val affectedTiles: Set<Tile>) : Incident(id, tick) {
 
     override fun execute() {
-        val tiles = mutableListOf<Fertile>()
-        for (tile in affectedTiles) {
-            val tileToAdd = tile.asFertile() ?: continue
-            tiles.add(tileToAdd)
-        }
-        tiles.forEach { tile -> tile.drought = true }
-        Logger.incidentExecuted(id, this, tiles.map { it.id }.sorted())
+        val actuallyAffectedTiles = mutableListOf<Tile>()
+        affectedTiles.forEach { if (it.asFertile()?.drought == false) actuallyAffectedTiles.add(it) }
+        actuallyAffectedTiles.forEach { if (it is Fertile) it.drought = true }
+        Logger.incidentExecuted(id, this, actuallyAffectedTiles.map { it.id }.sorted())
     }
     override fun toString(): String {
         return "DROUGHT"

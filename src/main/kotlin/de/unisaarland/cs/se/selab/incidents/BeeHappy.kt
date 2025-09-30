456d82d24cfe40be974c8a0d762687a3e6e036c7
Julian Gromov <jugr00003@stud.uni-saarland.de>
2025-09-30 15:19:04 +0200

    fix(BeeHappy) not on Drought and unsown Plant

--- src/main/kotlin/de/unisaarland/cs/se/selab/incidents/BeeHappy.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/incidents/BeeHappy.kt
@@ -16,7 +16,13 @@ class BeeHappy(
 ) : Incident(id, tick) {
 
     override fun execute() {
-        val pollinatableTiles = affectedTiles.filter { it.asFertile()?.plant?.pollinateable(yearTick) == true }
+        val pollinatableTiles = affectedTiles.filter {
+            it.asFertile()?.let { fertile ->
+                !fertile.hasDrought() &&
+                    fertile.plant.isSown() &&
+                    fertile.plant.pollinateable(yearTick)
+            } == true
+        }
         pollinatableTiles.forEach { if (it is Fertile) it.plant.addPollination(this) }
         Logger.incidentExecuted(id, this, pollinatableTiles.map { it.id }.sortedBy { it })
     }

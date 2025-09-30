ef9b91f7721943bf22dc8eca8da1912d06d5f3ad
Leonard.Fetzer <lefe00007@stud.uni-saarland.de>
2025-09-30 14:32:49 +0200

    animalAttack2

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/SystemTestRegistration.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/SystemTestRegistration.kt
@@ -19,6 +19,7 @@ import de.unisaarland.cs.se.selab.systemtest.selab25.cloudtests.CloudTestSmall
 import de.unisaarland.cs.se.selab.systemtest.selab25.cloudtests.Floodgates
 import de.unisaarland.cs.se.selab.systemtest.selab25.everythingTests.BigBigTestOctober1
 import de.unisaarland.cs.se.selab.systemtest.selab25.incidentTests.AnimalAttackTest
+import de.unisaarland.cs.se.selab.systemtest.selab25.incidentTests.AnimalAttackTestStacking
 import de.unisaarland.cs.se.selab.systemtest.selab25.incidentTests.CloudCreationTest
 import de.unisaarland.cs.se.selab.systemtest.selab25.incidentTests.CloudCreationTestLargeRadius
 import de.unisaarland.cs.se.selab.systemtest.selab25.incidentTests.CloudCreationTestNoTileToAffect
@@ -76,6 +77,7 @@ object SystemTestRegistration {
         testSuite.registerTest(BigBigTestOctober1())
         testSuite.registerTest(FallowSowPlanPotato())
         testSuite.registerTest(AnimalAttackTest())
+        testSuite.registerTest(AnimalAttackTestStacking())
     }
 
     /**

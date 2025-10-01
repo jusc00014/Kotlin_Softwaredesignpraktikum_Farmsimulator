e69e641f992e52deb9bc892b2ec286f4502454a4
Laurenz Adchayan <laad00002@stud.uni-saarland.de>
2025-10-01 13:51:25 +0200

    system test for BeeHappy

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/incidentTests/WeirdBeeHappyTest.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/incidentTests/WeirdBeeHappyTest.kt
@@ -19,6 +19,13 @@ class WeirdBeeHappyTest : TestExtension() {
     override val startYearTick = 7
 
     override suspend fun run() {
-        skipUntilLogType(LogLevel.IMPORTANT, "Incident:")
+        skipUntilLogType(LogLevel.IMPORTANT, "Simulation Statistics")
+        assertCurrentLine("[IMPORTANT] Simulation Statistics: Farm 0 collected 0 g of harvest.")
+        listOf("POTATO", "WHEAT", "OAT", "PUMPKIN", "APPLE", "GRAPE", "ALMOND", "CHERRY").forEach { plant ->
+            assertNextLine("[IMPORTANT] Simulation Statistics: Total amount of $plant harvested: 0 g.")
+        }
+        assertNextLine(
+            "[IMPORTANT] Simulation Statistics: Total harvest estimate still in fields and plantations: 1934032 g."
+        )
     }
 }

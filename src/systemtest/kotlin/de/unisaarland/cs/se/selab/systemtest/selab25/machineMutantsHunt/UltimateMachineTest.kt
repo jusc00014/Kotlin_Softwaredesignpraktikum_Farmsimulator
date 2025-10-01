ba90fbbd50c95cf1b44b2e091ac47f92ee370dbf
Leonard.Fetzer <lefe00007@stud.uni-saarland.de>
2025-10-01 20:36:41 +0200

    MachineSyTest

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/machineMutantsHunt/UltimateMachineTest.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/machineMutantsHunt/UltimateMachineTest.kt
@@ -1,23 +1,28 @@
 package de.unisaarland.cs.se.selab.systemtest.selab25.machineMutantsHunt
 
+import de.unisaarland.cs.se.selab.systemtest.selab25.utils.LogLevel
 import de.unisaarland.cs.se.selab.systemtest.selab25.utils.TestExtension
 
 /**
  * Detekt
  */
 class UltimateMachineTest : TestExtension() {
-    override val name = "UltimateMachineTest"
-    override val description = "Tests machine behavior."
+    override val name = "NoWayHomeTest"
+    override val description = "Tests machine movement behavior."
 
-    override val farms = "machineTravelTest/farms.json"
-    override val scenario = "machineTravelTest/scenario.json"
-    override val map = "machineTravelTest/map.json"
+    override val farms = "machineMutantsHunt/farms.json"
+    override val scenario = "machineMutantsHunt/scenario.json"
+    override val map = "machineMutantsHunt/map.json"
 
     override val logLevel = "DEBUG"
     override val maxTicks = 1
-    override val startYearTick = 1
+    override val startYearTick = 16
 
     override suspend fun run() {
-        return
+        skipUntilLogType(LogLevel.IMPORTANT, "Farm Action:")
+        assertCurrentLine("[IMPORTANT] Farm Action: Machine 0 performs HARVESTING on tile 0 for 1 days.")
+        assertNextLine("[IMPORTANT] Farm Harvest: Machine 0 has collected 800000 g of ALMOND harvest.")
+        assertNextLine("[IMPORTANT] Farm Machine: Machine 0 is finished but failed to return.")
+        assertNextLine("[IMPORTANT] Farm: Farm 0 finished its actions.")
     }
 }

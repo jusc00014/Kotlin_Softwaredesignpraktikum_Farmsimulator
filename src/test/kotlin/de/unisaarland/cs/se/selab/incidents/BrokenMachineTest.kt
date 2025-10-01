e6a9ff55cc7bb2c183e743943b0de3c61773e19f
Leonard.Fetzer <lefe00007@stud.uni-saarland.de>
2025-10-01 19:20:50 +0200

    BrokenMachineUnit

---
+++ src/test/kotlin/de/unisaarland/cs/se/selab/incidents/BrokenMachineTest.kt
@@ -0,0 +1,20 @@
+package de.unisaarland.cs.se.selab.incidents
+
+import de.unisaarland.cs.se.selab.board.Coordinate
+import de.unisaarland.cs.se.selab.board.Tile
+import de.unisaarland.cs.se.selab.board.TileType
+import de.unisaarland.cs.se.selab.farms.Machine
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class BrokenMachineTest {
+
+    @Test
+    fun test1() {
+        val tile = Tile(0, Coordinate(0, 0), null, false, 0, TileType.ROAD)
+        val machine = Machine(0, listOf(), listOf(), 10, tile, 0, false)
+        val incident = BrokenMachine(0, 0, 3, machine)
+        incident.execute()
+        assertEquals(3, machine.brokenFor)
+    }
+}

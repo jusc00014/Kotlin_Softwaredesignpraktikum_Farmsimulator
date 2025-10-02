f3ebd810f5d498eac37646ff9f8ac68c73d97dd5
Leonard.Fetzer <lefe00007@stud.uni-saarland.de>
2025-10-02 09:25:35 +0200

    MachineTest4

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/machineMutantsHunt/MachineTest4.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/machineMutantsHunt/MachineTest4.kt
@@ -1,5 +1,6 @@
 package de.unisaarland.cs.se.selab.systemtest.selab25.machineMutantsHunt
 
+import de.unisaarland.cs.se.selab.systemtest.selab25.utils.LogLevel
 import de.unisaarland.cs.se.selab.systemtest.selab25.utils.TestExtension
 
 /**
@@ -18,6 +19,17 @@ class MachineTest4 : TestExtension() {
     override val startYearTick = 21
 
     override suspend fun run() {
-        return
+        skipUntilLogType(LogLevel.IMPORTANT, "Farm:")
+        assertCurrentLine("[IMPORTANT] Farm: Farm 0 starts its actions.")
+        assertNextLine(
+            "[DEBUG] Farm: Farm 0 has the following active sowing plans it intends to pursue in this tick: ."
+        )
+        assertNextLine("[IMPORTANT] Farm Action: Machine 0 performs CUTTING on tile 0 for 1 days.")
+        assertNextLine("[IMPORTANT] Farm Action: Machine 0 performs CUTTING on tile 3 for 1 days.")
+        assertNextLine("[IMPORTANT] Farm Action: Machine 0 performs CUTTING on tile 5 for 1 days.")
+        assertNextLine("[IMPORTANT] Farm Machine: Machine 0 is finished and returns to the shed at 2.")
+        assertNextLine("[IMPORTANT] Farm Action: Machine 2 performs CUTTING on tile 6 for 1 days.")
+        assertNextLine("[IMPORTANT] Farm Machine: Machine 2 is finished and returns to the shed at 2.")
+        assertNextLine("[IMPORTANT] Farm: Farm 0 finished its actions.")
     }
 }
